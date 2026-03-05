131f2ef69a5e1d9950ab38aa879a73e17ee2f541
haurani <ismahaurani2023@gmail.com>
2026-03-05 22:12:59 +0700

    Add files via upload

---
+++ Variablesamples.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LAB-SI-PC
+ */
+public class Variablesamples {
+  public static void main(String[]args){
+      //deklarasi a tipe data dengan nama variable
+      //result dan tipe data boolean
+      boolean result;
+      
+      //deklarasi tipe data dengan nama variabel
+      //option dan tipe data char
+      char option;
+      option = 'c'; // menandai 'c' sebagai option
+      
+      //deklarasi tipe data dengan nama variabel
+      //grade, double tipe data dan telah diinisialisasi
+      //to 0.0
+      double grade = 0.0;
+ //system.out.println("result");
+ System.out.println("option");
+  System.out.println("grade");
+  }  
+}
+
+
